d7585461fc10a4414ca0c1ff3cbbd07e41951fe8
wang ying <598512076@qq.com>
2021-12-16 23:44:23 +0800

    fix umirc

--- .umirc.ts
+++ .umirc.ts
@@ -1,17 +1,19 @@
 import { defineConfig } from 'dumi'
 
+const repo = 'smart-taro-seed'
+
 export default defineConfig({
   title: 'Smart-taro-seed',
   favicon: 'https://img.icons8.com/ultraviolet/2x/year-of-monkey.png',
   logo: 'https://img.icons8.com/ultraviolet/2x/year-of-monkey.png',
-  base: '/smart-taro-seed',
-  publicPath: '/smart-taro-seed/',
+  // base: `/${repo}/`,
+  // publicPath: `./${repo}/`,
   outputPath: 'docs-dist',
-  history: { type: 'hash' },
   mode: 'site',
+  hash: true,
   exportStatic: {}, // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
   resolve: {
-    includes: ['docs', 'components'],
+    includes: ['docs', 'packages'],
   },
   navs: [
     null,
