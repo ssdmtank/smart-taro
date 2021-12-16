2684f3581f4b38b1126a77883bcddebb7177de0c
wang ying <598512076@qq.com>
2021-12-16 23:19:10 +0800

    fix2

--- .github/.umirc.ts
+++ .github/.umirc.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'dumi'
 
 export default defineConfig({
-  title: 'Monki UI',
+  title: 'Smart-taro-seed',
   favicon: 'https://img.icons8.com/ultraviolet/2x/year-of-monkey.png',
   logo: 'https://img.icons8.com/ultraviolet/2x/year-of-monkey.png',
   base: '/smart-taro-seed',
   publicPath: '/smart-taro-seed/',
   outputPath: 'docs-dist',
+  history: { type: 'hash' },
   mode: 'site',
   exportStatic: {}, // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
   resolve: {
